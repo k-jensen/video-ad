399331b9f601288f44894db6a5e63ed49225031f
Kimberley Jensen <kimberley.jensen@laerdal.com>
2020-01-22 14:49:08 +0100

    Use webpack to bundle code
    - Bundle code with webpack
    - Ignore public folder
    - Change default video source

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -6,7 +7,11 @@ app.set('view engine', 'pug')
 app.set('views', './views')
 
 app.get("/", function(req, res) {
-  res.render('index', { title: 'Video AD' })
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/other", function(req, res) {
+  res.render('other', { title: 'Other page' })
 });
 
 app.use(express.static(__dirname + '/public'));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            { test: /\.png$/, loader: 'file-loader' },
+            { test: /\.pug$/, loader: 'pug-loader' }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            //   favicon: 'favicon.ico',
+            template: 'src/templates/index.pug',
+        }),
+        new CopyPlugin([
+            { from: 'assets', to: 'assets' }
+        ]),
+    ]
+};
